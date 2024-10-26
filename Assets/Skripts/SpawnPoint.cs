dac78ec723e1df62af4284cae7b8b524f4496b7e
Konstantin Protasov <kostyapk3@gmail.com>
2024-10-26 13:56:37 +0300

    RefactoredCode

--- Assets/Skripts/SpawnPoint.cs
+++ Assets/Skripts/SpawnPoint.cs
@@ -1,20 +1,25 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Collider))]
 public class SpawnPoint : MonoBehaviour
 {
     public bool IsFree { get; private set; }
 
-    private void Start() => IsFree = true;
+    private void Start()
+    {
+        GetComponent<Collider>().isTrigger = true;
+        IsFree = true;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.TryGetComponent<Resource>(out _))
+        if (other.TryGetComponent<Resource>(out _))
             IsFree = false;
     }
-    
+
     private void OnTriggerExit(Collider other)
     {
-        if(other.TryGetComponent<Resource>(out _))
+        if (other.TryGetComponent<Resource>(out _))
             IsFree = true;
     }
 }
